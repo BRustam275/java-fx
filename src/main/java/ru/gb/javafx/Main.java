dac0fec50a18c66dd94f6a787561712885cc329d
Rustam <r.bagautdinov275@gmail.com>
2022-06-15 17:34:11 +0500

    Homework6

--- src/main/java/ru/gb/javafx/Main.java
+++ src/main/java/ru/gb/javafx/Main.java
@@ -4,13 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Dialog;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class Main extends Application{
+    static Stage mainStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
         primaryStage.setTitle("Сетевой чат 1.0.0");
         primaryStage.setScene(new Scene(root, 350, 700));
---
+++ src/main/java/ru/gb/javafx/Server.java
@@ -0,0 +1,52 @@
+package ru.gb.javafx;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        Socket socket;
+        try (ServerSocket serverSocket = new ServerSocket(8112)) {
+            socket = serverSocket.accept();
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+            addNewThread(inputStream);
+            sendFromServerConsole(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addNewThread(DataInputStream inputStream) {
+        new Thread(() -> {
+            try {
+                while (true) {
+                    String string = inputStream.readUTF();
+                    System.out.println(string);
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }).start();
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    private static void sendFromServerConsole(DataOutputStream outputStream) {
+        try {
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String message = scanner.nextLine();
+                outputStream.writeUTF(message);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
